0c6198e736f573d76749545b681d7bc6a0fb48f8
mouaad <mouadessaghie7@gmail.com>
2024-11-28 22:55:58 +0100

    html

---
+++ idex.js
@@ -0,0 +1,15 @@
+// const mouaad = document.querySelector('#mouadd')
+
+// // console.log(mouaad)
+
+// mouaad.addEventListener('click', ()=>{
+//     alert("hello java ")
+// })
+
+var x = 4
+if(x === 4){
+    var x = 2
+    console.log(x);
+    
+}
+console.log(x)
