ce039cdbc62ca6f4be7dd266059c4e7f6164ab0f
mouaad <mouadessaghie7@gmail.com>
2024-12-02 09:45:26 +0100

    complate-nav

--- Home/script.js
+++ Home/script.js
@@ -1,7 +1,12 @@
-const search_bar = document.querySelectorAll(".search-section")
+const more_setting = document.querySelector(".log-sign")
+const link = document.querySelector(".more_sett")
+const delete_stting = document.querySelector(".account-icon")
 
-search_bar.forEach(element => {
-    element.addEventListener("click", ()=>{
-        search_bar.style.backgroundColor = "green"
-    })
-});
+console.log(delete_stting);
+
+
+link.addEventListener("click", ()=>{
+    // more_setting.style.transition = "2s"
+    more_setting.classList.toggle("active")
+})
+
